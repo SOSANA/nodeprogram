67a3dca90a812242483481a6abeab165b1b610a3
sosana <thesosana@gmail.com>
2015-05-05 02:17:42 +0000

    created book prototype

--- JavaScript Fundamental Exercise/book.js
+++ JavaScript Fundamental Exercise/book.js
@@ -5,16 +5,32 @@
  * Remembers the number of the last page read
 */
 
+function Book(name, lastPageRead) {
+    this.name = name;
+    this.lastPageRead = 0;
+};
+
+Book.prototype.read = function(pages) {
+    this.lastPageRead += pages;
+};
+
+var practicalNode = new Book('Practical Node.js');
+practicalNode.read(101);
+console.log('last page read for ' + practicalNode.name + ' is ' + practicalNode.lastPageRead);
+
+/*
 var book = function(name) {
     return {
         name: name,
-        lastPageRead: 1,
+        lastPageRead: 0,
         read: function(pages) {
             this.lastPageRead += pages
         }
     }
 }
 
-var practicalNode = book('Practical Node.js');
+
+var practicalNode = new book('Practical Node.js');
 practicalNode.read(101);
 console.log('last page read for ' + practicalNode.name + ' is ' + practicalNode.lastPageRead);
+*/
