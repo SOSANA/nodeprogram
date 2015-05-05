cd171ab2da99af07e9cb259ae7d6bf0025af145a
sosana <thesosana@gmail.com>
2015-05-05 17:57:34 +0000

    adding exercise files for Node.js fundamentals exercise

---
+++ Lecture 8: Node.js Fundamentals Exercise/app.js
@@ -0,0 +1,18 @@
+/**
+* Created with nodeprogram.
+* User: sosana
+* Date: 2015-05-05
+* Time: 03:01 AM
+* To change this template use Tools | Templates.
+*/
+
+var slice = require('.slicer');
+var arr = [1,2,3];
+console.log(slice.f(arr,2))
+
+
+/*
+var module = require('./module')
+var arr = [1, 2, 3]
+console.log(module.f(arr,2)
+*/
---
+++ Lecture 8: Node.js Fundamentals Exercise/slicer.js
@@ -0,0 +1,10 @@
+/**
+* Created with nodeprogram.
+* User: sosana
+* Date: 2015-05-05
+* Time: 03:01 AM
+* To change this template use Tools | Templates.
+*/
+exports.f = function(arr, num){
+  return arr.slice(num);
+};
