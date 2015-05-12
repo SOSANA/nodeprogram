cc9882a4d5381fad5cf24119027c290294bd2463
sosana <thesosana@gmail.com>
2015-05-12 02:31:12 +0000

    updating comments

--- Lecture_16: helloWorldHerokuDemo/app.js
+++ Lecture_16: helloWorldHerokuDemo/app.js
@@ -1,8 +1,16 @@
 /*
- * notes goes here
- * 
- * 
+ * Hello World Server
+ * Go to "Rapid Protoyping with JS" epub p.128-129 and review 6.1.2 Node.js Core Modules
  * 
 */
 
+var http = require('http');
+var port = process.env.PORT || 3000;
 
+var server = http.createServer(function(request, response) {
+    response.setHeader('Content-Type', 'text/plain'),
+    response.end('Hello World');
+});
+
+server.listen(port);
+console.log("Server Listening on Port 3000");
