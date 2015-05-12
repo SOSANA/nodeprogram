9b0e32758d4b9f9559e106aa7d65306146a0921d
sosana <thesosana@gmail.com>
2015-05-12 02:16:04 +0000

    created first hello world server example

--- Lecture_15: helloWorldDemo/app.js
+++ Lecture_15: helloWorldDemo/app.js
@@ -4,3 +4,13 @@
  * 
 */
 
+var http = require('http');
+var port = process.env.PORT || 3000;
+
+var server = http.createServer(function(request, response) {
+    response.setHeader('Content-Type', 'text/plain'),
+    response.end('Hello World');
+});
+
+server.listen(port);
+console.log("Server Listening on Port 3000");
