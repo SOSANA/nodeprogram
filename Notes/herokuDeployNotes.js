72d86c4bb4ae4e40db2b0cd52a6ebb00fc37c65b
sosana <thesosana@gmail.com>
2015-05-13 18:32:26 +0000

    updating notes on heroku deployment notes

---
+++ Notes/herokuDeployNotes.js
@@ -0,0 +1,8 @@
+/*
+ * Heroku Deployment notes
+ * 
+ * 1. Do a npm init - creates your package.json file
+ * 2. Create a ProcFile - determines how to start the process, basically just a command line. 
+ * (.js extendtion optional) ex web: node app.js 
+ * 
+*/
