4720d20b9dcdc1f7e0648bf270d0de567f251961
sosana <thesosana@gmail.com>
2015-05-21 01:09:55 +0000

    updating comments for api server

--- lecture.19-chatRestAPIServerDemo/mb-server.js
+++ lecture.19-chatRestAPIServerDemo/mb-server.js
@@ -1,75 +1,78 @@
 /*
-Rapid Prototyping with JS is a JavaScript and Node.js book that will teach you how to build mobile and web apps fast. 
-— Read more at http://rapidprototypingwithjs.com.
+ * Refer to PDF manual section "Chat REST API Server (Memory Store)"
+ * Please keep in mind that, this is a very simplified comparison of strings and not JavaScript objects. 
+ * So every space, quote and case matters. You could make the comparison “smarter” by parsing a string
+ * into a JSON object with: JSON.parse(str);
+ * 
 */
 
+// loads http module
 var http = require('http');
-//loads http module
+// usefull functions
 var util= require('util');
-//usefull functions
+// loads querystring module, we'll need it to serialize and deserialize objects and query strings
 var querystring = require('querystring');
-//laods querystring module, we'll need it to serialize and deserialize objects and query strings
 
+// this array will hold our messages
 var messages=[];
-//this array will hold our messages
+// sample message to test list method
 messages.push({
-  "name":"John",
-  "message":"hi"
+    "name":"John",
+    "message":"hi"
 });
-//sample message to test list method
 
-exports.server=http.createServer(function (req, res) {
-//creates server
-  if (req.method=="POST"&&req.url=="/messages/create.json") {
-    //if method is POST and URL is messages/ add message to the array
-    var message='';
-    req.on('data', function(data, msg){
-      console.log(data.toString('utf-8'));
-      message=exports.addMessage(data.toString('utf-8'));
-      //data is type of Buffer and must be converted to string with encoding UTF-8 first
-      //adds message to the array
-
-    })
+// creates server
+exports.server = http.createServer(function (req, res) {
+ 
+    // if method is POST and URL is messages/ add message to the array
+    if (req.method == "POST" && req.url == "/messages/create.json") {
+        // data is type of Buffer and must be converted to string with encoding UTF-8 first
+        // adds message to the array
+        var message='';
+        req.on('data', function(data, msg){
+        console.log(data.toString('utf-8'));
+        message=exports.addMessage(data.toString('utf-8'));  
+        })
     req.on('end', function(){
-      console.log('message', util.inspect(message, true, null));
-      console.log('messages:', util.inspect(messages, true, null));
-      //debugging output into the terminal
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      //sets the right header and status code
-      res.end(message);
-      //out put message, should add object id
-    })
-  } else
-  if (req.method=="GET"&&req.url=="/messages/list.json") {
-  //if method is GET and URL is /messages output list of messages
-    var body=exports.getMessages();
-    //body will hold our output
-    res.writeHead(200, {
-      'Content-Length': body.length,
-      'Content-Type': 'text/plain'
-    });
-    res.end(body);
-  }
-  else {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    //sets the right header and status code
-    res.end('Hello World\n');
-  };
-  console.log(req.method);
-
-  //outputs string with line end symbol
+        console.log('message', util.inspect(message, true, null));
+        // debugging output into the terminal
+        console.log('messages:', util.inspect(messages, true, null));
+        // sets the right header and status code
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        // out put message, should add object id
+        res.end(message);
+        })
+    } else
+        // if method is GET and URL is /messages output list of messages
+        if (req.method == "GET" && req.url == "/messages/list.json") {
+        // body will hold our output
+        var body = exports.getMessages();
+   
+        res.writeHead(200, {
+            'Content-Length': body.length,
+            'Content-Type': 'text/plain'
+        });
+        res.end(body);
+    } else {
+        // sets the right header and status code  
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World\n');
+    };
+    // outputs string with line end symbol  
+    console.log(req.method);
+// sets port and IP address of the server  
 }).listen(process.env.PORT || 1337);
-//sets port and IP address of the server
+
 console.log('Server running at http://127.0.0.1:1337/');
 
-
 exports.getMessages = function() {
-  return JSON.stringify(messages);
-  //output array of messages as a string/text
+    // output array of messages as a string/text
+    return JSON.stringify(messages);
 };
+
 exports.addMessage = function (data){
-  messages.push(querystring.parse(data));
-  //to convert string into JavaScript object we use parse/deserializer
-  return JSON.stringify(querystring.parse(data));
-  //output new message in JSON as a string
+    // to convert string into JavaScript object we use parse/deserializer
+    messages.push(querystring.parse(data));
+    // output new message in JSON as a string
+    return JSON.stringify(querystring.parse(data));
 };
