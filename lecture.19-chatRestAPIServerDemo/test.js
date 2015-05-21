e4bdcbf0c7465fa781c404d6353397d7f4eb121c
sosana <thesosana@gmail.com>
2015-05-21 01:23:20 +0000

    updating comment notes on testing with curl

--- lecture.19-chatRestAPIServerDemo/test.js
+++ lecture.19-chatRestAPIServerDemo/test.js
@@ -1,9 +1,19 @@
 /*
  * For the step-by-step tutorial, go to Chat REST API (memory store) in manual. 
  * The source code is at http://bit.ly/1usviBi. 
+ * 
  * To test the server, you can use CURL command: 
  * $ curl http://127.0.0.1:1337 
- * $ curl -X POST -d 'name=sosana&message=waz up' http://127.0.0.1:1337 
+ * 
+ * To see list of messages:
+ * $ curl localhost:1337/messages/list.json
+ * 
+ * To add new messages:
+ * $ curl -X POST -d 'name=sosana&message=waz up' http://127.0.0.1:1337/messages/create.json 
+ * 
+ * To check if messages worked
+ * $ curl localhost:1337/messages/list.json
+ * 
  * CURL is also available for Windows at http://curl.haxx.se/download.html. 
  * The UI/browser app for the Chat is in this folder: https://github.com/azat-co/rpjs/tree/master/backbone.
 
