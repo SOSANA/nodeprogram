1ca1461bae178ed6534e17377099b12938424ca9
sosana <thesosana@gmail.com>
2015-05-29 18:41:33 +0000

    creating new folder and files for oauth exercise

---
+++ oauth1.0/app.js
@@ -0,0 +1,9 @@
+/*
+ * node program manual p.68
+ * 
+ * Creating a small Express.js app that has a Sign in link that allows to authenticate users.
+ * 
+ * The app will be able to output Twitter user information after the authentication, and get 
+ * access toprotected Twitter resources on behalf of this particular user.
+ * 
+*/
