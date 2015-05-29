dd8fd397a9c3a4fb140072cb522d64336a37eabe
sosana <thesosana@gmail.com>
2015-05-29 01:24:34 +0000

    updating comments regarding rest api

--- restApiExpress/app.js
+++ restApiExpress/app.js
@@ -12,6 +12,18 @@
  * Developers have to install separate modules, except for express.static, Express.js 4.x. So to parse incoming 
  * information, we add body-parser.
  * 
+ * CURL data to make a POST request:
+ * $ curl -X POST -d "name=sosana" http://localhost:3000/collections/test13
+ * 
+ * Check this object either by using our REST API server:
+ * $ curl http://localhost:3000/collections/test13
+ * 
+ * GET requests also work in the browser. For example, open this link while your local server is running on port 3000 
+ * http://localhost:3000/collections/test13. Or if we don’t trust server results (why wouldn’t we? but let’s pretend 
+ * we don’t), we can open MongoDB shell ($ mongo) and type: > db.test13.find()
+ * 
+ * 
+ * 
 */
 
 // this line will give us a server object:
@@ -45,10 +57,14 @@ app.param('collectionName', function(req, res, next, collectionName){
     return next();
 });
 
+// Merely to be user-friendly, let’s put a root route with a message:
 app.get('/', function(req, res, next) {
     res.send('please select a collection, e.g., /collections/messages')
 });
 
+// Now the real work begins, here is how we retrieve a list of any items (first parameter is an empty
+// object {} which means any). The results will be capped at a limit of 10 and sorted by _id (second
+// parameter). The find() method returns a cursor, so we call toArray() to get the JavaScript/Node.js array:
 app.get('/collections/:collectionName', function(req, res, next) {
     req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results){
         if (e) return next(e);
@@ -56,6 +72,12 @@ app.get('/collections/:collectionName', function(req, res, next) {
     })
 });
 
+// Have you noticed a :collectionName string in the URL pattern parameter? This and the previous app.param() 
+// middleware is what gives us the req.collection object, which points to a specified collection in our database.
+// The object creating endpoint is slightly easier to grasp since we just pass the whole payload to the MongoDB. 
+// This method often called free JSON REST API because server and then the database accept any data structure. 
+// Parse.com and other Back-end as a Service providers pioneered the free JSON approach. In our Express.js app, 
+// we use req.body for this:
 app.post('/collections/:collectionName', function(req, res, next) {
     req.collection.insert(req.body, {}, function(e, results){
         if (e) return next(e);
@@ -63,6 +85,9 @@ app.post('/collections/:collectionName', function(req, res, next) {
     })
 });
 
+// Single object retrieval functions like findById and findOne are faster than find(), but they use a different 
+// interface (they return the object directly instead of a cursor). So please be aware of that. In addition, we’re 
+// extracting the ID from :id part of the path with req.params.id Express.js magic:
 app.get('/collections/:collectionName/:id', function(req, res, next) {
     req.collection.findById(req.params.id, function(e, result){
         if (e) return next(e);
@@ -70,6 +95,10 @@ app.get('/collections/:collectionName/:id', function(req, res, next) {
     })
 });
 
+// PUT request handler gets more interesting because update() doesn’t return the augmented object. Instead it returns 
+// us a count of affected objects. Also {$set:req.body} is a special MongoDB operator (operators tend to start with a 
+// dollar sign) that sets values. The second ‘ {safe:true, multi:false}‘ parameter is an object with options that tell 
+// MongoDB to wait for the execution before running the callback function and to process only one (first) item.
 app.put('/collections/:collectionName/:id', function(req, res, next) {
     req.collection.updateById(req.params.id, {$set: req.body}, {safe: true, multi: false}, function(e, result){
         if (e) return next(e);
@@ -77,6 +106,9 @@ app.put('/collections/:collectionName/:id', function(req, res, next) {
     })
 });
 
+// Finally, the DELETE HTTP method is processed by app.del(). In the request handler, we utilize removeById() which does 
+// exactly what it sounds like it should do, and takes an ID and a callback. Then, we output a custom JSON message success 
+// on the deletion:
 app.delete('/collections/:collectionName/:id', function(req, res, next) {
     req.collection.removeById(req.params.id, function(e, result){
         if (e) return next(e);
